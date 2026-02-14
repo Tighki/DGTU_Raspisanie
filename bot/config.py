00398b19fc8abd58a3deda8822bd49ff3f441cd1
Tighki <115498774+Tighki@users.noreply.github.com>
2026-02-14 21:13:14 +0300

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -4,9 +4,9 @@ import os
 class Config:
     def __init__(self):
         self.bot_token: str = self._get_env('BOT_TOKEN', '')
-        self.mongo_uri: str = self._get_env('MONGO_URI', 'mongodb://mongo:3zzc8t1bephxkkvn@192.168.1.182:27019/')
-        self.mongo_db: str = self._get_env('MONGO_DB', 'ras')
-        self.mongo_collection: str = self._get_env('MONGO_COLLECTION', 'sessions')
+        self.mongo_uri: str = self._get_env('MONGO_URI', '')
+        self.mongo_db: str = self._get_env('MONGO_DB', '')
+        self.mongo_collection: str = self._get_env('MONGO_COLLECTION', '')
         
         if not self.bot_token:
             raise ValueError("BOT_TOKEN обязателен для работы бота")
